a680d889d2d7e5a7d20dda2f62deac592f4a865d
jeswanthsantham <jeswanthsantham@gmail.com>
2026-01-04 22:47:33 +0530

    Day 21 Docker Basics

---
+++ spring-restapi-docker/src/main/java/com/greetapp/SpringRestapiDockerApplication.java
@@ -0,0 +1,13 @@
+package com.greetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestapiDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestapiDockerApplication.class, args);
+	}
+
+}
